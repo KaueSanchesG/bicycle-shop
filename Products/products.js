0f3332e1b40bdfc23b56977c87028470ebe470e7
KaueSanchesG <kauesanches123@gmail.com>
2024-05-10 19:04:47 -0300

    feat: func add to cart working

--- Products/products.js
+++ Products/products.js
@@ -2,9 +2,9 @@ function createTableBody() {
   const produtos = JSON.parse(localStorage.getItem("produtos")) || [];
   const productsHTML = document.getElementById("products");
 
-  //   productsHTML.innerHTML = "";
+  productsHTML.innerHTML = ""; // Limpar o conteúdo antes de adicionar os produtos
 
-  produtos.forEach(function (produto) {
+  produtos.forEach(function (produto, index) {
     productsHTML.innerHTML += `
         <div class="product-item">
             <div class="img-container">
@@ -13,7 +13,7 @@ function createTableBody() {
             <div class="text-container">
                 <div class="add-cart">
                     <p>Valor: ${produto.precoVenda}</p>
-                    <button type="button">+ Carrinho</button>
+                    <button onclick="addToCart(${index})" type="button">+ Carrinho</button>
                 </div>
                 <div class="description-container">${produto.descricao}</div>
             </div>
@@ -24,6 +24,18 @@ function createTableBody() {
 
 createTableBody();
 
-function addToCart(descr) {
-  console.log("Produto adicionado ao carrinho:", desc);
+function addToCart(index) {
+  const produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  if (index >= 0 && index < produtos.length) {
+    const produto = produtos[index];
+    cart.push(produto);
+    produtos.splice(index, 1);
+    localStorage.setItem("produtos", JSON.stringify(produtos));
+    localStorage.setItem("cart", JSON.stringify(cart));
+    location.reload();
+  } else {
+    console.error("Índice inválido:", index);
+  }
 }
