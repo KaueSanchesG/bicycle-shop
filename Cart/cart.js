4a69c80638060dea8a6cb888b3ee575d23f66bb0
KaueSanchesG <kauesanches123@gmail.com>
2024-05-17 19:06:40 -0300

    feat: css implementation and storage logic done

--- Cart/cart.js
+++ Cart/cart.js
@@ -1,32 +1,3 @@
-function createCartTable() {
-  const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
-  const tableBody = document.getElementById("cartTableBody");
-
-  tableBody.innerHTML = "";
-
-  cartItems.forEach(function (item, index) {
-    tableBody.innerHTML += `
-        <tr class="table-row">
-            <td class="table-data">
-                <div class="center-row">
-                    <img class="img-item"
-                    src="${item.imagem}"
-                    alt="Imagem do produto"
-                    />
-                </div>
-            </td>
-            <td class="table-data">${item.descricao}</td>
-            <td class="table-data">${item.precoVenda}</td>
-            <td class="table-data">
-                <div class="center-row">
-                    <button class="remove-btn" onclick="removeFromCart(${index})" type="button">- Remover</button>
-                </div>
-            </td>
-        </tr>
-      `;
-  });
-}
-
 function removeFromCart(index) {
   const cart = JSON.parse(localStorage.getItem("cart")) || [];
   const produtos = JSON.parse(localStorage.getItem("produtos")) || [];
@@ -44,4 +15,100 @@ function removeFromCart(index) {
   }
 }
 
-createCartTable();
+function closePopup() {
+  const popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
+
+function createCartTable() {
+  const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+  const tableBody = document.getElementById("cartTableBody");
+
+  tableBody.innerHTML = "";
+
+  cartItems.forEach(function (item, index) {
+    tableBody.innerHTML += `
+      <tr class="table-row">
+        <td class="table-data">
+          <div class="center-row">
+            <img class="img-item" src="${item.imagem}" alt="Imagem do produto" />
+          </div>
+        </td>
+        <td class="table-data">${item.descricao}</td>
+        <td class="table-data">${item.precoVenda}</td>
+        <td class="table-data">
+          <div class="center-row">
+            <button class="remove-btn" onclick="removeFromCart(${index})" type="button">- Remover</button>
+          </div>
+        </td>
+      </tr>
+    `;
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  function openPopup() {
+    const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+    if (cartItems.length === 0) {
+      alert("Não há itens no carrinho. Adicione itens antes de comprar.");
+      return;
+    }
+    const popup = document.getElementById("popup");
+    popup.style.display = "block";
+  }
+
+  function finalizarCompra() {
+    event.preventDefault();
+
+    const vendedor = document.getElementById("vendedor").value.trim();
+    const formaPagamento = document
+      .getElementById("forma-pagamento")
+      .value.trim();
+
+    if (!vendedor || !formaPagamento) {
+      alert("Por favor, preencha os campos antes de finalizar a compra.");
+      return;
+    }
+
+    const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+    if (cartItems.length === 0) {
+      alert("Não há itens no carrinho. Adicione itens antes de comprar.");
+      return;
+    }
+
+    const controleEstoque = {
+      dataVenda: new Date().toLocaleDateString(),
+      itensVendidos: cartItems,
+      descricao: document.getElementById("descricao").value.trim(),
+      vendedor: vendedor,
+      formaPagamento: formaPagamento,
+    };
+
+    const estoque = JSON.parse(localStorage.getItem("estoque")) || [];
+    estoque.push(controleEstoque);
+    localStorage.setItem("estoque", JSON.stringify(estoque));
+
+    localStorage.removeItem("cart");
+
+    closePopup();
+
+    alert("Compra finalizada com sucesso!");
+
+    document.getElementById("vendedor").value = "";
+    document.getElementById("forma-pagamento").value = "";
+    document.getElementById("descricao").value = "";
+
+    createCartTable();
+  }
+
+  const comprarBtn = document.querySelector(".pay-btn");
+  comprarBtn.addEventListener("click", openPopup);
+
+  const finalizarBtn = document.getElementById("finalizar-btn");
+  finalizarBtn.addEventListener("click", finalizarCompra);
+
+  const finalizarForm = document.getElementById("finalizar-form");
+  finalizarForm.addEventListener("submit", finalizarCompra);
+
+  createCartTable();
+});
